e29b2535b8d4326bee5c320ce2e03b31c01a3b10
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2024-05-29 15:45:08 +0530

    changing state dynamically

---
+++ kotine_practice/StateChange.kt
@@ -0,0 +1,72 @@
+package com.example.stylingtext
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.stylingtext.ui.theme.StylingTExtTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier) {
+    val color = remember {
+            mutableStateOf(Color.Yellow)
+        }
+    Box(modifier = modifier
+        .background(Color.DarkGray)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+
+        } )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewColorBox() {
+    ColorBox(Modifier.fillMaxSize())
+}
+
+
+
+
+
+
+
